b1d1cf19fc84234144083e10c3fb04085c2dae39
Ryan Manchester <ryanm17@gmail.com>
2021-04-13 21:17:29 -0400

    boilerplate code for ActionProvider, MessageParser, and config files

---
+++ src/ActionProvider.js
@@ -0,0 +1,11 @@
+
+
+class ActionProvider  {
+  constructor(createChatBotMessage, setStateFunc, createClientMessage) {
+    this.createChatBotMessage = createChatBotMessage;
+    this.setState = setStateFunc;
+    this.createClientMessage = createClientMessage;
+  }
+}
+
+export default ActionProvider;
---
+++ src/config.js
@@ -0,0 +1,7 @@
+import { createChatBotMessage } from 'react-chatbot-kit';
+
+const config = {
+  initialMessages: [createChatBotMessage('Hello World')]
+}
+
+export default config
