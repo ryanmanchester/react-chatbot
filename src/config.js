4d42bf657880f3984ffd03c65e6a1887d2acc6a5
Ryan Manchester <ryanm17@gmail.com>
2021-05-27 12:52:19 -0400

    extracts resource links into separate file

--- src/config.js
+++ src/config.js
@@ -1,6 +1,8 @@
 import { createChatBotMessage } from 'react-chatbot-kit';
 import LearningOptions from './components/LearningOptions';
 import LinkList from './components/LinkList';
+import {anxietyLinks, depressionLinks} from './links/resourceLinks';
+
 
 const config = {
   botName: "Mental Health Bot",
@@ -15,47 +17,14 @@ const config = {
       widgetName: "anxietyLinks",
       widgetFunc: (props) => <LinkList {...props} />,
       props: {
-        options: [
-          {
-            text: "What is Anxiety?",
-            url:  "https://www.psychologytoday.com/us/basics/anxiety",
-            id: 1
-          },
-          {
-            text: "How I Healed My Lifelong Anxiety",
-            url:
-              "https://theholisticpsychologist.com/how-i-healed-my-life-long-anxiety/",
-            id: 2,
-          },
-          {
-            text: "Natural Approaches to Anxiety",
-            url: "https://www.psychologytoday.com/intl/basics/anxiety/natural-approaches-anxiety",
-            id: 3,
-          },
-        ]
+        options: anxietyLinks
       }
     },
     {
       widgetName: "depressionLinks",
       widgetFunc: (props) => <LinkList {...props} />,
       props: {
-        options: [
-          {
-          text:'What is Depression?',
-          url: 'https://www.psychologytoday.com/intl/basics/depression',
-          id: 1
-        },
-        {
-          text:'Integrative Mental Health',
-          url: 'https://www.huffpost.com/entry/integrative-mental-health_b_354332',
-          id: 2
-        },
-        {
-          text: 'Is Depression Good For You?',
-          url: 'https://fullpermissionliving.blogspot.com/2008/03/is-depression-good-for-you.html',
-          id: 3
-        }
-      ]
+        options: depressionLinks
       }
     },
   ],
---
+++ src/links/resourceLinks.js
@@ -0,0 +1,36 @@
+export const anxietyLinks = [
+  {
+    text: "What is Anxiety?",
+    url:  "https://www.psychologytoday.com/us/basics/anxiety",
+    id: 1
+  },
+  {
+    text: "How I Healed My Lifelong Anxiety",
+    url:
+      "https://theholisticpsychologist.com/how-i-healed-my-life-long-anxiety/",
+    id: 2,
+  },
+  {
+    text: "Natural Approaches to Anxiety",
+    url: "https://www.psychologytoday.com/intl/basics/anxiety/natural-approaches-anxiety",
+    id: 3,
+  }
+]
+
+export const depressionLinks = [
+  {
+  text:'What is Depression?',
+  url: 'https://www.psychologytoday.com/intl/basics/depression',
+  id: 1
+},
+{
+  text:'Integrative Mental Health',
+  url: 'https://www.huffpost.com/entry/integrative-mental-health_b_354332',
+  id: 2
+},
+{
+  text: 'Is Depression Good For You?',
+  url: 'https://fullpermissionliving.blogspot.com/2008/03/is-depression-good-for-you.html',
+  id: 3
+}
+]
